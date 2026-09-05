96d9dd2672e12eefef36f59b3aa422adbb00f83a
STH-DELFI <58655344+anit3k@users.noreply.github.com>
2026-09-05 11:51:37 +0200

    Restructure solution with Clean Architecture layers (Domain, Application, Infrastructure, API)

---
+++ src/ShoppingCart.Application/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ShoppingCart.Application;
+
+public class Class1
+{
+
+}
---
+++ tests/ShoppingCart.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingCart.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
